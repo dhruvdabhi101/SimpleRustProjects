e9e6d1dc69de9164b5672844c0e2f65a597cf44d
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2023-03-29 00:27:20 +0530

    add: more opcode execution

--- chip8/chip8_core/src/lib.rs
+++ chip8/chip8_core/src/lib.rs
@@ -169,6 +169,54 @@ impl Emu {
                 let nn = (op * 0xFF) as u8; 
                 self.v_reg[x] = self.v_reg[x].wrapping_add(nn);
             }
+            (8, _, _, 0) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                self.v_reg[x] = self.v_reg[y];
+            }
+            (8, _, _, 1) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                self.v_reg[x] != self.v_reg[y];
+            }
+            (8, _, _, 4) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                
+                let (new_vx, carry) = self.v_reg[x].overflowing_add(self.v_reg[y]);
+                let new_vf = if carry {1} else {0};
+
+                self.v_reg[x] = new_vx;
+                self.v_reg[0xF] = new_vf;
+            }
+            (8, _, _, 5) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                
+                let (new_vx, borrow) = self.v_reg[x].overflowing_sub(self.v_reg[y]);
+                let new_vf = if borrow {0} else {1};
+
+                self.v_reg[x] = new_vx;
+                self.v_reg[0xF] = new_vf;
+            }
+            (8, _, _, 6) => {
+                let x = digit2 as usize;
+                let lab = self.v_reg[x] & 1; 
+                self.v_reg[x] >>= 1 ;
+                self.v_reg[0xF] = lab;
+            }
+
+            (8, _, _, 7) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                
+                let (new_vx, borrow) = self.v_reg[y].overflowing_sub(self.v_reg[x]);
+                let new_vf = if borrow {0} else {1};
+
+                self.v_reg[x] = new_vx;
+                self.v_reg[0xF] = new_vf;
+            }
+
         }
 
     }
